35ec3276e2def8f25782c27a9f2a89aaf9eb0692
ronanren <renouxpro@gmail.com>
2020-05-07 23:05:00 +0200

    update edition des voitures

--- src/main/java/fr/iutlannion/admin/PageAdmin.java
+++ src/main/java/fr/iutlannion/admin/PageAdmin.java
@@ -26,6 +26,11 @@ public class PageAdmin extends Stage {
     private Label label8 = new Label("Mot de passe :");
     private Label label9 = new Label("Tarif :");
     private Label label10 = new Label("Km parcourus :");
+    private Label label11 = new Label("Immatriculation :");
+    private Label label12 = new Label("Marque :");
+    private Label label13 = new Label("Type essence :");
+    private Label label14 = new Label("Couleur :");
+    private Label label15 = new Label("Type :");
 
     private ObservableList<Admin> admins = FXCollections.observableArrayList(Admins.getInstance().getListAdmin());
     private ListView<Admin> listViewAdmin = new ListView<Admin>(admins);
@@ -45,6 +50,11 @@ public class PageAdmin extends Stage {
     private TextField textFieldMotdepasse = new TextField();
     private TextField textFieldTarif = new TextField();
     private TextField textFieldKmParcourus = new TextField();
+    private TextField textFieldImmatriculation = new TextField();
+    private TextField textFieldMarque = new TextField();
+    private TextField textFieldTypeEssence = new TextField();
+    private TextField textFieldCouleur = new TextField();
+    private TextField textFieldType = new TextField();
 
     private String TypeCurrentPersonne = null;
 
@@ -56,8 +66,18 @@ public class PageAdmin extends Stage {
                 buttonSauvegarder.setDisable(false);
                 textFieldTarif.setVisible(false);
                 textFieldKmParcourus.setVisible(false);
+                textFieldImmatriculation.setVisible(false);
+                textFieldMarque.setVisible(false);
+                textFieldTypeEssence.setVisible(false);
+                textFieldCouleur.setVisible(false);
+                textFieldType.setVisible(false);
                 label9.setVisible(false);
                 label10.setVisible(false);
+                label11.setVisible(false);
+                label12.setVisible(false);
+                label13.setVisible(false);
+                label14.setVisible(false);
+                label15.setVisible(false);
                 textFieldNom.setText(listViewAdmin.getSelectionModel().getSelectedItem().getNom());
                 textFieldPrenom.setText(listViewAdmin.getSelectionModel().getSelectedItem().getPrenom());
                 textFieldEmail.setText(listViewAdmin.getSelectionModel().getSelectedItem().getEmail());
@@ -72,8 +92,18 @@ public class PageAdmin extends Stage {
                 buttonSauvegarder.setDisable(false);
                 textFieldTarif.setVisible(false);
                 textFieldKmParcourus.setVisible(false);
+                textFieldImmatriculation.setVisible(false);
+                textFieldMarque.setVisible(false);
+                textFieldTypeEssence.setVisible(false);
+                textFieldCouleur.setVisible(false);
+                textFieldType.setVisible(false);
                 label9.setVisible(false);
                 label10.setVisible(false);
+                label11.setVisible(false);
+                label12.setVisible(false);
+                label13.setVisible(false);
+                label14.setVisible(false);
+                label15.setVisible(false);
                 textFieldNom.setText(listViewPassager.getSelectionModel().getSelectedItem().getNom());
                 textFieldPrenom.setText(listViewPassager.getSelectionModel().getSelectedItem().getPrenom());
                 textFieldEmail.setText(listViewPassager.getSelectionModel().getSelectedItem().getEmail());
@@ -88,8 +118,18 @@ public class PageAdmin extends Stage {
                 buttonSauvegarder.setDisable(false);
                 textFieldTarif.setVisible(true);
                 textFieldKmParcourus.setVisible(true);
+                textFieldImmatriculation.setVisible(true);
+                textFieldMarque.setVisible(true);
+                textFieldTypeEssence.setVisible(true);
+                textFieldCouleur.setVisible(true);
+                textFieldType.setVisible(true);
                 label9.setVisible(true);
                 label10.setVisible(true);
+                label11.setVisible(true);
+                label12.setVisible(true);
+                label13.setVisible(true);
+                label14.setVisible(true);
+                label15.setVisible(true);
                 textFieldNom.setText(listViewConducteur.getSelectionModel().getSelectedItem().getNom());
                 textFieldPrenom.setText(listViewConducteur.getSelectionModel().getSelectedItem().getPrenom());
                 textFieldEmail.setText(listViewConducteur.getSelectionModel().getSelectedItem().getEmail());
@@ -130,11 +170,24 @@ public class PageAdmin extends Stage {
         listViewAdmin.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         listViewPassager.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         listViewConducteur.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        listViewAdmin.setMaxHeight(380);
+        listViewPassager.setMaxHeight(380);
+        listViewConducteur.setMaxHeight(380);
         buttonSauvegarder.setDisable(true);
         textFieldTarif.setVisible(false);
         textFieldKmParcourus.setVisible(false);
+        textFieldImmatriculation.setVisible(false);
+        textFieldMarque.setVisible(false);
+        textFieldTypeEssence.setVisible(false);
+        textFieldCouleur.setVisible(false);
+        textFieldType.setVisible(false);
         label9.setVisible(false);
         label10.setVisible(false);
+        label11.setVisible(false);
+        label12.setVisible(false);
+        label13.setVisible(false);
+        label14.setVisible(false);
+        label15.setVisible(false);
 
         /*
          * Colonne, Ligne ronan@email.com
@@ -148,21 +201,32 @@ public class PageAdmin extends Stage {
         gridPane.add(listViewConducteur, 2, 2);
         gridPane.add(label5, 0, 4);
         gridPane.add(label6, 1, 4);
-        gridPane.add(label7, 0, 6);
-        gridPane.add(label8, 1, 6);
-        gridPane.add(label9, 0, 8);
-        gridPane.add(label10, 1, 8);
+        gridPane.add(label7, 2, 4);
+        gridPane.add(label8, 0, 6);
+        gridPane.add(label9, 1, 6);
+        gridPane.add(label10, 2, 6);
+        gridPane.add(label11, 0, 8);
+        gridPane.add(label12, 1, 8);
+        gridPane.add(label13, 2, 8);
+        gridPane.add(label14, 0, 10);
+        gridPane.add(label15, 1, 10);
         gridPane.add(textFieldNom, 0, 5);
         gridPane.add(textFieldPrenom, 1, 5);
-        gridPane.add(textFieldEmail, 0, 7);
-        gridPane.add(textFieldMotdepasse, 1, 7);
-        gridPane.add(textFieldTarif, 0, 9);
-        gridPane.add(textFieldKmParcourus, 1, 9);
-        gridPane.add(buttonSauvegarder, 3, 7);
+        gridPane.add(textFieldEmail, 2, 5);
+        gridPane.add(textFieldMotdepasse, 0, 7);
+        gridPane.add(textFieldTarif, 1, 7);
+        gridPane.add(textFieldKmParcourus, 2, 7);
+        gridPane.add(textFieldImmatriculation, 0, 9);
+        gridPane.add(textFieldMarque, 1, 9);
+        gridPane.add(textFieldTypeEssence, 2, 9);
+        gridPane.add(textFieldCouleur, 0, 11);
+        gridPane.add(textFieldType, 1, 11);
+        gridPane.add(buttonSauvegarder, 3, 11);
 
         gridPane.setHalignment(label2, HPos.CENTER);
         gridPane.setHalignment(label3, HPos.CENTER);
         gridPane.setHalignment(label4, HPos.CENTER);
+        gridPane.setHalignment(buttonSauvegarder, HPos.RIGHT);
 
         return gridPane;
     }
--- src/main/java/fr/iutlannion/manager/Voitures.java
+++ src/main/java/fr/iutlannion/manager/Voitures.java
@@ -7,19 +7,19 @@ import fr.iutlannion.core.Voiture;
 public class Voitures {
 	private ArrayList<Voiture> voitures;
 	private static Voitures instance;
-	
+
 	public Voitures() {
 		this.voitures = new ArrayList<Voiture>();
 		instance = this;
 	}
-	
+
 	public static Voitures getInstance() {
 		if (instance == null) {
 			return new Voitures();
 		}
 		return instance;
 	}
-	
+
 	public void add(Voiture v) {
 		if (!this.voitures.contains(v)) {
 			this.voitures.add(v);
@@ -27,7 +27,7 @@ public class Voitures {
 			System.out.println("Cette voiture est déjà présente.");
 		}
 	}
-	
+
 	public void remove(Voiture v) {
 		if (this.voitures.contains(v)) {
 			this.voitures.remove(v);
