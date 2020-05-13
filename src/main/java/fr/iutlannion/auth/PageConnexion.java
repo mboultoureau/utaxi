357260b2ab88620dd91f1ba1db1fbd3ff412f9a9
ronanren <renouxpro@gmail.com>
2020-05-13 10:23:14 +0200

    update notification failure connexion

--- src/main/java/fr/iutlannion/auth/PageConnexion.java
+++ src/main/java/fr/iutlannion/auth/PageConnexion.java
@@ -11,6 +11,7 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import fr.iutlannion.manager.Conducteurs;
 import fr.iutlannion.manager.Passagers;
@@ -22,6 +23,7 @@ public class PageConnexion extends Stage {
     private Label label1 = new Label("Connexion à PStage");
     private Label label2 = new Label("Nom :");
     private Label label3 = new Label("Mot de passe :");
+    private Label label4 = new Label("");
 
     private Button buttonAnnuler = new Button("Annuler");
     private Button buttonOk = new Button("OK");
@@ -35,17 +37,22 @@ public class PageConnexion extends Stage {
             public void handle(MouseEvent event) {
                 if (Conducteurs.getInstance().verifConnexion(textField.getText(), passwordField.getText()) != null) {
                     System.out.println("Conducteur");
+                    label4.setText("");
                 } else if (Passagers.getInstance().verifConnexion(textField.getText(),
                         passwordField.getText()) != null) {
                     System.out.println("Passager");
+                    label4.setText("");
                 } else if (Admins.getInstance().verifConnexion(textField.getText(), passwordField.getText()) != null) {
+                    label4.setText("");
                     Window.getInstance().gotoPage("admin");
+                } else if (textField.getText().compareTo("") == 0 || passwordField.getText().compareTo("") == 0) {
+                    label4.setText("Erreur, champ vide");
                 } else {
-                    System.out.println("Email/mot de passe invalide");
+                    label4.setText("Email/mot de passe invalide");
                 }
             }
         }));
-        
+
         buttonAnnuler.setOnMouseClicked((new EventHandler<MouseEvent>() {
             public void handle(MouseEvent event) {
                 Window.getInstance().gotoPage("mainMenu");
@@ -64,6 +71,7 @@ public class PageConnexion extends Stage {
 
         buttonAnnuler.setPrefWidth(70);
         buttonOk.setPrefWidth(70);
+        label4.setTextFill(Color.RED);
 
         gridPane.add(label1, 0, 0, 2, 1);
         gridPane.add(label2, 0, 1);
@@ -72,8 +80,10 @@ public class PageConnexion extends Stage {
         gridPane.add(passwordField, 1, 2);
         gridPane.add(buttonOk, 1, 3);
         gridPane.add(buttonAnnuler, 1, 3);
+        gridPane.add(label4, 1, 4, 2, 1);
         GridPane.setHalignment(buttonAnnuler, HPos.RIGHT);
         GridPane.setHalignment(label1, HPos.CENTER);
+        GridPane.setHalignment(label4, HPos.CENTER);
 
         return gridPane;
     }
