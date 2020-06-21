515176c797b17e251525a49edbfebe7e2d347a8a
WelcomeToZhaun <43570451+WelcomeToZhaun@users.noreply.github.com>
2020-06-21 22:16:06 +0200

    javadoc classe jour

--- src/main/java/fr/iutlannion/manager/Jour.java
+++ src/main/java/fr/iutlannion/manager/Jour.java
@@ -2,11 +2,14 @@ package fr.iutlannion.manager;
 
 import java.util.ArrayList;
 
+
+//classe représentant les horaires d'une journée
 public class Jour {
     private String nom;
     private int heureD;
     private int heureF;
 
+    //initialisation de la journée
     public Jour(String nomJ, int heureDJ, int heureFJ){
         nom = nomJ;
         heureD = heureDJ;
