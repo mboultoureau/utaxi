d6a2d6b319d4aa79b3704d0f31f7364a177f7372
ErwanLeflot <erwan.leflot@gmail.com>
2020-06-21 21:49:35 +0200

    JavaDoc P.Conducteur

--- src/main/java/fr/iutlannion/dashboard/PageConducteur.java
+++ src/main/java/fr/iutlannion/dashboard/PageConducteur.java
@@ -3,7 +3,6 @@ package fr.iutlannion.dashboard;
 import fr.iutlannion.core.Window;
 import fr.iutlannion.manager.Conducteur;
 import fr.iutlannion.manager.Passager;
-import fr.iutlannion.manager.Requete;
 import fr.iutlannion.manager.Utilisateurs;
 import fr.iutlannion.map.Adresse;
 import fr.iutlannion.map.AdresseView;
@@ -23,7 +22,6 @@ import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -33,12 +31,13 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class PageConducteur extends Stage {
-
+	//Création d'utilisateur courant pour l'affichage ainsi que l'affichage en lui meme
 	private Conducteur c = (Conducteur) Utilisateurs.getPersonneCourante();
 	private BorderPane root = new BorderPane();
 	private AdresseView adresseView = new AdresseView();
 
 	// Header
+	//affichage
 	private HBox header = new HBox();
 	private Button backButton = new Button("Deconnexion");
 	private Label title = new Label("Page Conducteur - Bienvenue " + Utilisateurs.getPersonneCourante().getPrenom());
@@ -49,7 +48,6 @@ public class PageConducteur extends Stage {
 	// Left Side
 
 	private GridPane leftSide = new GridPane();
-	private Label soon = new Label("");
 	private Button actif = new Button("actif");
 	private Label requeteLabel = new Label("");
 
@@ -58,16 +56,12 @@ public class PageConducteur extends Stage {
 	private MapOptions mapOptions = new MapOptions();
 	private MapView map;
 	private Icon icon = new Icon("img/taxi.png", 40, 20);
-	private Icon iconSelected = new Icon("img/taxi-selected.png", 40, 20);
 
+	//création de markeur et récuperation d'adresse du taxi
 	private Marker markerCurrentPosition = new Marker(c.getMarker().getCoords());
-
 	Adresse adresse = adresseView.getAdresse();
-	/*
-	 * map.moveMarker(markerLocationWant, adresse.getCoords());
-	 * map.traceRoute(markerCurrentPosition, markerLocationWant);
-	 */
-
+	
+	//fonction permettant de changer le statut du taxi si le taxi est inactif il ne pourra pas conduire de personne
 	public void changementStatus() {
 		if (c.actif == true) {
 			actif.setText("Inactif");
@@ -78,19 +72,21 @@ public class PageConducteur extends Stage {
 		}
 
 	}
-
+	//création de la page 
 	public PageConducteur() {
+		//changement de statut et changement du bouton grace à la fonction
 		actif.setOnMouseClicked((new EventHandler<MouseEvent>() {
 			public void handle(MouseEvent event) {
 				changementStatus();
 			}
 		}));
+		//Retour lors du clic sur deconnexion à la page de connexion
 		backButton.setOnMouseClicked((new EventHandler<MouseEvent>() {
 			public void handle(MouseEvent event) {
 				Window.getInstance().gotoPage("connexion");
 			}
 		}));
-
+		// Creation du menu déroulant pour acceder aux pages edition de voiture et profil
 		selectionPage.setValue("Tableau de bord");
 
 		selectionPage.valueProperty().addListener(new ChangeListener() {
@@ -106,14 +102,14 @@ public class PageConducteur extends Stage {
 				}
 			}
 		});
-
+		//Si il n'y a pas de requete alors ce message s'affiche
 		requeteLabel.setText("Pas de requête en cours");
 		// System.out.println(Utilisateurs.getRequete().toString());
 
 	}
-
+	//Creation de la page lors de l'appelle de la classe creerContenu()
 	public Parent creerContenu() {
-
+		//notion de "decorum"
 		// Header
 		header.setMinWidth(640);
 		header.setPadding(new Insets(0, 20, 0, 20));
@@ -138,11 +134,11 @@ public class PageConducteur extends Stage {
 
 		// Left Side
 		leftSide.setHgap(15);
-		leftSide.add(soon, 0, 0);
+		
 		leftSide.setMinWidth(300);
 		leftSide.add(actif, 0, 1);
 		leftSide.add(requeteLabel, 0, 2);
-
+		//creation de la carte avec les marqueurs ainsi que les coordonnées de base de la ville et le zoom
 		// Map
 		mapOptions.setCoordinates(new LatLng(47.2186371, -1.5541362));
 		mapOptions.setZoom(13);
